590ec0c1c9878b562ab70097d035c2f1d0a54f38
Jimmy Israkhan <jimmy.israkhan@gmail.com>
2019-07-19 15:13:15 +0700

    Created 'iFrame' view component to use as iframe container.

---
+++ src/views/iFrame.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <div>
+            <input type="text"
+                @keyup.enter="Go"
+                v-model="externalPage"
+            >
+            <button
+                @click="Go"
+            >
+                Go
+            </button>
+        </div>
+        <vue-i-frame
+            class="vue-i-frame"
+            :src="pageLoader"
+            @load="onLoad"
+            @document-load="onDocumentLoad">
+        </vue-i-frame>
+    </div>
+</template>
+
+<script>
+import VueIFrame from 'vue-friendly-iframe';
+
+export default {
+    data() {
+        return {
+            externalPage: '',
+            pageLoader: 'about:blank',
+        }
+    },
+    components: {
+        VueIFrame,
+    },
+    methods: {
+        Go() {
+            this.pageLoader = this.externalPage;
+        },
+        onLoad() {
+
+        },
+        onDocumentLoad() {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.vue-i-frame {
+    width: 100%;
+    min-height: 600px;
+    height: 100%;
+    display: block;
+
+}
+
+iframe {
+    width: 100%;
+    min-height: 600px;
+    height: 100%;
+    display: block;
+
+}
+</style>
